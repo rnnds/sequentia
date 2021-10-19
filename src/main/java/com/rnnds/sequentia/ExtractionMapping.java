874d283f220bd3e92a8bdc6bf9fdecd0d57aacb0
Ronan Silva <ronan.silva@quintoandar.com.br>
2021-10-19 11:13:26 -0300

    Using lombok

--- src/main/java/com/rnnds/sequentia/ExtractionMapping.java
+++ src/main/java/com/rnnds/sequentia/ExtractionMapping.java
@@ -1,26 +1,14 @@
 package com.rnnds.sequentia;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 class ExtractionMapping {
 
     private final String field;
     private final Class<?> type;
     private final int length;
 
-    public ExtractionMapping(final String field, final Class<?> type, final int length) {
-        this.field = field;
-        this.type = type;
-        this.length = length;
-    }
-
-    public String getField() {
-        return field;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public Class<?> getType() {
-        return type;
-    }
 }
--- src/test/java/com/rnnds/sequentia/Person.java
+++ src/test/java/com/rnnds/sequentia/Person.java
@@ -1,11 +1,16 @@
 package com.rnnds.sequentia;
 
-import com.google.common.base.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import static com.google.common.base.Objects.equal;
 import static com.rnnds.sequentia.PadMode.LEFT;
-import static com.rnnds.sequentia.PadMode.RIGHT;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Person {
 
     @SequentialMapping(length = 8)
@@ -16,56 +21,5 @@ public class Person {
 
     @SequentialMapping(length = 10, pad = LEFT, padCharacter = '0')
     private Long income;
-
-    public Person() {
-    }
-
-    public Person(final String name, final int age, final Long income) {
-        this.name = name;
-        this.age = age;
-        this.income = income;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(final int age) {
-        this.age = age;
-    }
-
-    public Long getIncome() {
-        return income;
-    }
-
-    public void setIncome(final Long income) {
-        this.income = income;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(this.name, this.age, this.income);
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        } else {
-            if (obj instanceof Person) {
-                Person that = (Person) obj;
-                return equal(this.name, that.name) && equal(this.age, that.age) && equal(this.income, that.income);
-            }
-            return false;
-        }
-    }
 
 }
